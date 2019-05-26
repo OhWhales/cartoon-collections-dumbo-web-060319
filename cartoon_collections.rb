b6fbdc2d7ae9cc1d053787c345c7012812503888
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-26 05:38:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def roll_call_dwarves(arr)# code an argument here
   counter = 1
   count = 0
   while (count < arr)
+    x=counter.to_s
     puts "#{counter}. #{arr[count]}"
     counter = counter + 1
     count= count + 1
