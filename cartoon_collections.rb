909dc7acf3739f026b7b092fa1c33be638afc47e
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-26 05:34:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr)# code an argument here
   # Your code here
+  for x in arr 
+    puts x 
+  end 
 end
 
 def summon_captain_planet# code an argument here
