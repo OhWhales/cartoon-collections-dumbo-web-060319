54b0d2f60cf42afeef63a7fcda7db5f6b395aff4
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-26 05:48:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def long_planeteer_calls(arr)# code an argument here
   return bool 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
